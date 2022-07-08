821e974e1c549cdc564c5f42a22524ea0d8889ad
Andrew Guerrero <juvedon@Andrews-iMac.local>
2022-07-08 15:36:28 -0400

    added a keep score method and a log round method that displays the results after each round

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const gameChoices = ["rock", "paper", "scissors"];
+const winners = [];
+
 
 function game() {
-    playRound();
+    for(let i = 1; i <=5; i++){
+        playRound(i);
+    }
+    keepScore();
 }
+
+function playRound(round){
+    let playerSelection = playerChoice();
+    let computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner); //will add the winner into the winners array
+    logRound(playerSelection, computerSelection, winner, round);
+} 
 
 function computerChoice() {
   return gameChoices[~~(Math.random() * gameChoices.length)];
@@ -27,15 +40,6 @@ function playerChoice (){
     return input;
 }
 
-function playRound(computerS, playerS){
-    let playerSelection = playerChoice();
-    let computerSelection = computerChoice();
-    const winner = checkWinner(playerSelection, computerSelection);
-    console.log("Player Chose:" + playerSelection);
-    console.log("Computer Chose:" + computerSelection);
-    console.log(winner);
-} 
-
 // validates if the player's choice is in our array of gameChoices
 function validatePlayerChoice(playerChoice){
     return gameChoices.includes(playerChoice); //returns true if it is, false if it's not
@@ -49,10 +53,29 @@ function checkWinner(choiceP, choiceC) {
       (choiceP === "paper" && choiceC == "rock") ||
       (choiceP === "scissors" && choiceC == "paper")
     ) {
-      return "Player Wins!";
+      return "Player Wins";
     } else {
-      return "Computer Wins!";
+      return "Computer Wins";
     }
-  }
+}
 
-playRound();
+function keepScore(){
+    let playerWins = winners.filter((item) => item == "Player Wins").length; //Will loop through the winners array searching for "Player Wins"
+    let computerWins = winners.filter((item) => item == "Computer Wins").length;
+    let ties = winners.filter((item) => item == "Tie").length;
+    console.log("Player Wins: " + playerWins);
+    console.log("Computer Wins: " + computerWins);
+    console.log("Ties: " + ties);
+}
+
+function logRound(playerChoice, computerChoice, winner, round){
+    console.log("Round: " + round);
+    console.log("Player Chose: " + playerChoice);
+    console.log("Computer Chose: " + computerChoice);
+
+    console.log("Winner: " + winner);
+    
+}
+
+
+game();
